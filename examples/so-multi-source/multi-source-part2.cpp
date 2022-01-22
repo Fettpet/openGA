4ded5d2f8e92e75a96a2a6d4a4b58c78c4950e3f
Sebastian Hahn <sebastian.hahn@hicknhack-software.com>
2022-01-22 21:10:55 +0100

    Chore: reformat all source code files
    Ticket: https://github.com/Fettpet/openGA/issues/5

--- examples/so-multi-source/multi-source-part2.cpp
+++ examples/so-multi-source/multi-source-part2.cpp
@@ -2,10 +2,7 @@
 // Mozilla Public License Version 2.0.
 
 #define OPENGA_EXTERN_LOCAL_VARS
-#include "openGA.hpp"
 #include "header.hpp"
+#include "openGA.hpp"
 
-void main2()
-{
-	run_test(false,false,0,"run-test-2");
-}
+void main2() { run_test(false, false, 0, "run-test-2"); }
